1be0622cf47dc76213b69b48590a2a82b564a275
Pedro Gutierrez <pgutierrez@signal.co>
2021-11-25 09:10:41 -0500

    added compose file

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker run --name mitmproxy --rm -d -p 8080:8080 -p 8081:8081 -v $(pwd):/data  mitmproxy/mitmproxy mitmweb --web-host 0.0.0.0 --web-port 8081 -s /data/scripts/redirect.py -s /data/scripts/modify_response.py
+docker-compose -f proxy.yml up -d
 docker logs mitmproxy -f
