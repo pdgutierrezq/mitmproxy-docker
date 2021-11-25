c11d26558f3744c70690b6e7c3e02b11341c83d9
Pedro Gutierrez <pgutierrez@signal.co>
2021-11-25 18:28:15 -0500

    auto-commit

--- ec2/test.sh
+++ ec2/test.sh
@@ -5,13 +5,13 @@
 git add .
 git commit -m "auto-commit"
 git push
-ssh -i "rb-pb-dev-ecs-auto.pem" ec2-user@ec2-3-15-4-198.us-east-2.compute.amazonaws.com <<'ENDSSH'
-   #commands to run on remote host
-  cd mitmproxy-docker
-   docker ps
-   docker stop mitmproxy
-   git fetch
-   git pull
-   ./run.sh
-ENDSSH
+#ssh -i "rb-pb-dev-ecs-auto.pem" ec2-user@ec2-3-15-4-198.us-east-2.compute.amazonaws.com <<'ENDSSH'
+#   #commands to run on remote host
+#  cd mitmproxy-docker
+#   docker ps
+#   docker stop mitmproxy
+#   git fetch
+#   git pull
+#   ./run.sh
+#ENDSSH
 
