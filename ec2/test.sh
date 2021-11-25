a5956615ab7c6ec4ac206d422a402fb925a8bb88
Pedro Gutierrez <pgutierrez@signal.co>
2021-11-25 18:29:57 -0500

    auto-commit

--- ec2/test.sh
+++ ec2/test.sh
@@ -4,7 +4,7 @@
 #apt-get install curl
 git add .
 git commit -m "auto-commit"
-git push
+#git push
 #ssh -i "rb-pb-dev-ecs-auto.pem" ec2-user@ec2-3-15-4-198.us-east-2.compute.amazonaws.com <<'ENDSSH'
 #   #commands to run on remote host
 #  cd mitmproxy-docker
