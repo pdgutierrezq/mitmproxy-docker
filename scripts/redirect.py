4cb54f48e95a08cddd7bec129ea863bea9b87482
Pedro Gutierrez <pgutierrez@signal.co>
2021-11-25 00:04:12 -0500

    updated redirection

--- scripts/redirect.py
+++ scripts/redirect.py
@@ -8,7 +8,7 @@ def request(flow: http.HTTPFlow) -> None:
     # which is useful in transparent mode where we usually only have the IP
     # otherwise.
     if flow.request.pretty_host == "10.5.8.216":
-      if flow.request.path == 'CustomerConditions':
+      if flow.request.path == '/CustomerConditions':
         flow.request.host = "bb-stg-alb-ecs-ext-902953221.us-east-2.elb.amazonaws.com"
         flow.request.port = 80
         flow.request.path = '/castlemock/mock/soap/project/7cGqrI/CustomerConditionsInquirySvcPort'
