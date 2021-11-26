672108ae92329c97e3c2a5758e828c1a8c0fe8ea
Pedro Gutierrez <pgutierrez@signal.co>
2021-11-25 23:39:02 -0500

    auto-commit

--- ec2/castlemock.sh
+++ ec2/castlemock.sh
@@ -5,5 +5,9 @@ ssh -i "rb-pb-dev-ecs-auto.pem" "ec2-user@$EC2" <<'ENDSSH'
   git fetch
   git pull
   cd data/projects/castlemock
-  curl -X POST "http://10.5.10.86/castlemock/api/rest/core/project/soap/import" -H "accept: */*" -H "Authorization: YWRtaW46YWRtaW4x" -H "Content-Type: multipart/form-data" -F "file=@project-soap-7cGqrI.xml;type=text/xml"
+#  curl -X POST "http://10.5.10.86/castlemock/api/rest/core/project/soap/import" -H "accept: */*" -H "Authorization: YWRtaW46YWRtaW4x" -H "Content-Type: multipart/form-data" -F "file=@project-soap-7cGqrI.xml;type=text/xml"
+  curl --location --request POST 'http://10.5.10.86/castlemock/api/rest/core/project/soap/import' \
+  --header 'accept: */*' \
+  --header 'Authorization: Basic YWRtaW46YWRtaW4x' \
+  --form 'file=@"/Users/pedro.gutierrez/Downloads/project-soap-7cGqrI.xml"'
 ENDSSH
