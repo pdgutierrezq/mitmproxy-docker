a9f256ce552e0579c99b859166e404f342316e54
Pedro Gutierrez <pgutierrez@signal.co>
2021-11-25 22:42:40 -0500

    auto-commit

--- ec2/test.sh
+++ ec2/test.sh
@@ -2,9 +2,11 @@
 #docker exec -it proxy /bin/bash
 #apt-get update
 #apt-get install curl
+EC2_0='ec2-user@ec2-3-15-4-198.us-east-2.compute.amazonaws.com'
+EC2_1='ec2-user@ec2-3-135-221-17.us-east-2.compute.amazonaws.com'
 git commit -am "auto-commit"
 git push
-ssh -i "rb-pb-dev-ecs-auto.pem" ec2-user@ec2-3-15-4-198.us-east-2.compute.amazonaws.com <<'ENDSSH'
+ssh -i "rb-pb-dev-ecs-auto.pem $EC2" <<'ENDSSH'
    #commands to run on remote host
   cd mitmproxy-docker
    docker ps
