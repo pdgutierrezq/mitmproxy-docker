668c2738fa2851d2f6dd4bea04fb214980de9ff7
Pedro Gutierrez <pgutierrez@signal.co>
2021-11-25 23:46:35 -0500

    auto-commit

--- ec2/castlemock.sh
+++ ec2/castlemock.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 sh ./git.sh
+CASTLEMOCK_HOST=localhost
 ssh -i "rb-pb-dev-ecs-auto.pem" "ec2-user@$EC2" <<'ENDSSH'
   cd mitmproxy-docker
   git fetch
   git pull
   cd data/projects/castlemock
-  curl --location --request DELETE 'http://10.5.10.86/castlemock/api/rest/core/project/soap/7cGqrI' \
+  curl --location --request DELETE 'http://localhost/castlemock/api/rest/core/project/soap/7cGqrI' \
   --header 'accept: */*' \
   --header 'Authorization: Basic YWRtaW46YWRtaW4='
-  curl --location --request DELETE 'http://10.5.10.86/castlemock/api/rest/core/project/rest/WJBLM5' \
+  curl --location --request DELETE 'http://localhost/castlemock/api/rest/core/project/rest/WJBLM5' \
   --header 'accept: */*' \
   --header 'Authorization: Basic YWRtaW46YWRtaW4='
-  curl --location --request POST 'http://10.5.10.86/castlemock/api/rest/core/project/soap/import' \
+  curl --location --request POST 'http://localhost/castlemock/api/rest/core/project/soap/import' \
   --header 'accept: */*' \
   --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
   --form 'file=@"project-soap-7cGqrI.xml"'
-  curl --location --request POST 'http://10.5.10.86/castlemock/api/rest/core/project/rest/import' \
+  curl --location --request POST 'http://localhost/castlemock/api/rest/core/project/rest/import' \
   --header 'accept: */*' \
   --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
   --form 'file=@"project-rest-WJBLM5.xml"'
