2f83b54f461d3cb2ca41a00b899d9bfe152c554f
Pedro Gutierrez <pgutierrez@signal.co>
2021-12-13 14:04:07 -0500

    auto-commit

--- data/proxy/scripts/redirect.py
+++ data/proxy/scripts/redirect.py
@@ -9,10 +9,10 @@ def request(flow: http.HTTPFlow) -> None:
     # otherwise.
 #     if flow.request.pretty_host == "10.5.10.169":
     if flow.request.pretty_host == "proxy":
-      path = flow.request.path.split('/',1)[1]
-#    host = host.split('/')[0]
-#    path = flow.request.path.replace(host,'')
-      ctx.log.info("Request domain: " + flow.request.pretty_host +
+      basePath=flow.request.path.split('/',1)
+      host = basePath[0]
+      path = basePath[1]
+      ctx.log.info("Output: domain: " + host +
                     ", path: " + path)
       if flow.request.path == '/192.168.135.28/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente':
         flow.request.host = '192.168.135.28'
--- ec2/connect.sh
+++ ec2/connect.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 ssh -i "rb-pb-dev-ecs-auto.pem" "ec2-user@$EC2"
+#docker logs -n 10 proxy
