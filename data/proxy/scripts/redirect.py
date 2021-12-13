de76342fce32f2bbb06d4ce6dfa3c803123bb3d5
Pedro Gutierrez <pgutierrez@signal.co>
2021-12-13 14:28:01 -0500

    auto-commit

--- data/proxy/scripts/redirect.py
+++ data/proxy/scripts/redirect.py
@@ -3,24 +3,32 @@ This example shows two ways to redirect flows to another server.
 """
 from mitmproxy import http, ctx
 
+
 def request(flow: http.HTTPFlow) -> None:
-    # pretty_host takes the "Host" header of the request into account,
-    # which is useful in transparent mode where we usually only have the IP
-    # otherwise.
-#     if flow.request.pretty_host == "10.5.10.169":
-    if flow.request.pretty_host == "proxy":
-      basePath=flow.request.path.split("/",1)
-      host = basePath[0]
-      path = basePath[1]
-      ctx.log.info("Output: domain: " + basePath[1] +
-                    ", path: " + basePath[2])
-      if flow.request.path == '/192.168.135.28/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente':
-        flow.request.host = '192.168.135.28'
-        flow.request.scheme = "https"
-        flow.request.port = 442
-        flow.request.path = '/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente'
-      if flow.request.path == '/mock':
-#         flow.request.host = "rb-dev-alb-ecs-ext-525169194.us-east-2.elb.amazonaws.com"
-        flow.request.host = "localhost"
-        flow.request.port = 80
-        flow.request.path = '/castlemock/mock/soap/project/7cGqrI/CustomerConditionsInquirySvcPort'
+  # pretty_host takes the "Host" header of the request into account,
+  # which is useful in transparent mode where we usually only have the IP
+  # otherwise.
+  #     if flow.request.pretty_host == "10.5.10.169":
+  if flow.request.pretty_host == "proxy":
+    basePath = BasePath(flow.request.path)
+    ctx.log.info("Output: domain: " + basePath.host +
+                 ", path: " + basePath.path)
+    if flow.request.path == '/192.168.135.28/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente':
+      flow.request.host = '192.168.135.28'
+      flow.request.scheme = "https"
+      flow.request.port = 442
+      flow.request.path = '/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente'
+    if flow.request.path == '/mock':
+      #         flow.request.host = "rb-dev-alb-ecs-ext-525169194.us-east-2.elb.amazonaws.com"
+      flow.request.host = "localhost"
+      flow.request.port = 80
+      flow.request.path = '/castlemock/mock/soap/project/7cGqrI/CustomerConditionsInquirySvcPort'
+
+class BasePath:
+  def __init__(self, path):
+    initialPath = path.split('/', 2)
+    self.host = initialPath[1]
+    self.path = initialPath[2]
+
+
+# print(BasePath('/192.168.135.28/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente').path)
