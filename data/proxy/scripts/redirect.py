7a8c7704a7d7ac9a8a941b272bb6ef7ba7e94a98
Pedro Gutierrez <pgutierrez@signal.co>
2021-12-13 12:24:00 -0500

    auto-commit

--- data/proxy/scripts/redirect.py
+++ data/proxy/scripts/redirect.py
@@ -9,14 +9,14 @@ def request(flow: http.HTTPFlow) -> None:
     # otherwise.
 #     if flow.request.pretty_host == "10.5.10.169":
     if flow.request.pretty_host == "proxy":
-    host = flow.request.path
-    host = host.split('/')[0]
-    path = flow.request.path.replace(host,'')
-      if flow.request.path == '/192.168.135.28/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente':
+#    host = flow.request.path
+#    host = host.split('/')[0]
+#    path = flow.request.path.replace(host,'')
+      if flow.request.path == '/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente':
         flow.request.host = '192.168.135.28'
         flow.request.scheme = "https"
         flow.request.port = 442
-        flow.request.path = '/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente'
+#         flow.request.path = '/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente'
       if flow.request.path == '/mock':
 #         flow.request.host = "rb-dev-alb-ecs-ext-525169194.us-east-2.elb.amazonaws.com"
         flow.request.host = "localhost"
