f98ebd3f88fc48053815937c45827e84d7d9ff48
Pedro Gutierrez <pgutierrez@signal.co>
2021-12-13 12:37:35 -0500

    auto-commit

--- data/proxy/scripts/redirect.py
+++ data/proxy/scripts/redirect.py
@@ -12,7 +12,7 @@ def request(flow: http.HTTPFlow) -> None:
 #    host = flow.request.path
 #    host = host.split('/')[0]
 #    path = flow.request.path.replace(host,'')
-      if flow.request.path == '/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente':
+      if flow.request.path == '/192.168.135.28/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente':
         flow.request.host = '192.168.135.28'
         flow.request.scheme = "https"
         flow.request.port = 442
