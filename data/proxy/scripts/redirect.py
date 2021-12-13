75389b71470eecb980ec13f84adb575871a519fa
Pedro Gutierrez <pgutierrez@signal.co>
2021-12-13 15:43:11 -0500

    auto-commit

--- data/proxy/scripts/redirect.py
+++ data/proxy/scripts/redirect.py
@@ -10,25 +10,44 @@ def request(flow: http.HTTPFlow) -> None:
   # otherwise.
   #     if flow.request.pretty_host == "10.5.10.169":
   if flow.request.pretty_host == "proxy":
-    basePath = BasePath(flow.request.path)
-    ctx.log.info("Output: domain: " + basePath.host +
-                 ", path: " + basePath.path)
+    request = Request(flow.request.path)
+    ctx.log.info("Output: " + request.toString())
     if flow.request.path == '/https://192.168.135.28:442/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente':
-      flow.request.host = basePath.host
-      flow.request.scheme = "https"
-      flow.request.port = 442
-      flow.request.path = basePath.path
+      flow.request.host = request.host
+      flow.request.scheme = request.scheme
+      flow.request.port = request.port
+      flow.request.path = request.path
     if flow.request.path == '/mock':
       #         flow.request.host = "rb-dev-alb-ecs-ext-525169194.us-east-2.elb.amazonaws.com"
       flow.request.host = "localhost"
       flow.request.port = 80
       flow.request.path = '/castlemock/mock/soap/project/7cGqrI/CustomerConditionsInquirySvcPort'
 
-class BasePath:
+class Request:
   def __init__(self, path):
-    initialPath = path.split('/', 2)
-    self.host = initialPath[1]
-    self.path = '/'+initialPath[2]
+    initialPath = path.split('/', 4)
+    self.scheme = initialPath[1].replace(':','')
+    if(self.scheme =='http'):
+      self.port="80"
+    else:
+      self.port="443"
+    self.host = initialPath[3]
+    splitHost=initialPath[3].split(':')
+    if(len(splitHost)>1):
+      self.host = splitHost[0]
+      self.port = splitHost[1]
+    self.path = '/'+initialPath[4]
+
+  def toString(self) -> None:
+    print("scheme:"+self.scheme)
+    print("host:"+self.host)
+    print("port:"+self.port)
+    print("path:"+self.path)
 
 
-# print(BasePath('/192.168.135.28/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente').path)
+
+
+
+print(Request('/https://192.168.135.28:442/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente').toString())
+print(Request('/https://192.168.135.28/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente').toString())
+print(Request('/http://192.168.135.28/PFBA_Crm31/sca/WSBA_Crm_consultarCondicionesCliente').toString())
