0b80b03d7e5186a9c3b06dc2c7885ed978f292ce
Pedro Gutierrez <pgutierrez@signal.co>
2022-01-21 16:10:49 -0500

    auto-commit

--- ec2/castlemock.sh
+++ ec2/castlemock.sh
@@ -20,39 +20,4 @@ import_project(){
   git pull
   cd data/projects/castlemock
   import_project soap 7cGqrI
-  curl --location --request DELETE "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/soap/7cGqrI" \
-  --header 'accept: */*' \
-  --header 'Authorization: Basic YWRtaW46YWRtaW4='
-  curl --location --request DELETE "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/rest/WJBLM5" \
-  --header 'accept: */*' \
-  --header 'Authorization: Basic YWRtaW46YWRtaW4='
-  curl --location --request DELETE "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/soap/WHtzrB" \
-  --header 'accept: */*' \
-  --header 'Authorization: Basic YWRtaW46YWRtaW4='
-  curl --location --request DELETE "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/soap/wn0iz3" \
-  --header 'accept: */*' \
-  --header 'Authorization: Basic YWRtaW46YWRtaW4='
-  curl --location --request POST "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/soap/import" \
-  --header 'accept: */*' \
-  --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
-  --form 'file=@"project-soap-7cGqrI.xml"'
-  curl --location --request POST "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/rest/import" \
-  --header 'accept: */*' \
-  --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
-  --form 'file=@"project-rest-WJBLM5.xml"'
-  curl --location --request POST "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/soap/import" \
-  --header 'accept: */*' \
-  --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
-  --form 'file=@"project-soap-WHtzrB.xml"'
-  curl --location --request POST "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/soap/import" \
-  --header 'accept: */*' \
-  --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
-  --form 'file=@"project-soap-wn0iz3.xml"'
-  curl --location --request DELETE "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/rest/a3ymXG" \
-  --header 'accept: */*' \
-  --header 'Authorization: Basic YWRtaW46YWRtaW4='
-  curl --location --request POST "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/rest/import" \
-  --header 'accept: */*' \
-  --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
-  --form 'file=@"project-rest-a3ymXG.xml"'
 ENDSSH
