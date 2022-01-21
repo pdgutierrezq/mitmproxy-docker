37b2cbe7e0c83026d5e799d2548f0f1c7f127dfb
Pedro Gutierrez <pgutierrez@signal.co>
2022-01-21 16:10:04 -0500

    auto-commit

--- ec2/castlemock.sh
+++ ec2/castlemock.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 sh ./git.sh
 ssh -i "rb-pb-dev-ecs-auto.pem" "ec2-user@$EC2" <<'ENDSSH'
+import_project(){
+   PROJECT_ID=$1
+   PROJECT_TYPE=$2
+   curl --location --request DELETE "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/$PROJECT_TYPE/$PROJECT_ID" \
+   --header 'accept: */*' \
+   --header 'Authorization: Basic YWRtaW46YWRtaW4='
+   curl --location --request POST "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/rest/import" \
+   --header 'accept: */*' \
+   --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
+   --form "file=@\"project-$PROJECT_TYPE-$PROJECT_ID.xml\""
+   RESULT="$PROJECT_ID imported correctly!!!"
+   echo $RESULT
+}
   CASTLEMOCK_HOST=localhost
   cd mitmproxy-docker
   git fetch
   git pull
   cd data/projects/castlemock
+  import_project soap 7cGqrI
   curl --location --request DELETE "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/soap/7cGqrI" \
   --header 'accept: */*' \
   --header 'Authorization: Basic YWRtaW46YWRtaW4='
