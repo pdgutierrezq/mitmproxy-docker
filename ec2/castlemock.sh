39817c67e18c62040730c68a54b9fb83fb717b7d
Pedro Gutierrez <pedro.gutierrez@avaldigitalabs.com>
2022-06-24 17:37:51 -0500

    :seeding: sorting mocks folder

--- ec2/castlemock.sh
+++ ec2/castlemock.sh
@@ -10,7 +10,7 @@ ssh -i "$KEY" "ec2-user@$EC2" <<'ENDSSH'
      curl --location --request POST "http://$CASTLEMOCK_HOST/castlemock/api/rest/core/project/$PROJECT_TYPE/import" \
      --header 'accept: */*' \
      --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
-     --form "file=@\"project-$PROJECT_TYPE-$PROJECT_ID.xml\""
+     --form "file=@\"exported-project-$PROJECT_TYPE-$PROJECT_ID.xml\""
      RESULT="$PROJECT_ID imported correctly!!!"
      echo $RESULT
   }
@@ -20,14 +20,17 @@ ssh -i "$KEY" "ec2-user@$EC2" <<'ENDSSH'
   git pull
   cd data/projects/castlemock
   import_project rest a3ymXG
+  import_project rest ETGtzo
+  import_project rest FuQbLm
+  import_project rest KNqM2U
+  import_project rest MvGKro
+  import_project rest mz8eSA
+  import_project rest qzx9pb
   import_project rest WJBLM5
   import_project soap 7cGqrI
+  import_project soap DNJ8nW
+  import_project soap E5KUlt
   import_project soap szT8NH
   import_project soap WHtzrB
   import_project soap wn0iz3
-  import_project rest mz8eSA
-  import_project soap E5KUlt
-  import_project soap DNJ8nW
-  import_project rest ETGtzo
-  import_project rest KNqM2U
 ENDSSH
