80a9744190ae93171e919e12d7eeb1c5aab0d964
Pedro Gutierrez <pedro.gutierrez@avaldigitalabs.com>
2022-08-08 10:24:47 -0500

    :sparkles:Added lines on install script to initiate docker after machine boot

--- ec2/install.sh
+++ ec2/install.sh
@@ -11,6 +11,8 @@ ssh -o StrictHostKeyChecking=no -i "$KEY" "ec2-user@$EC2" <<'ENDSSH'
   sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
   docker-compose version
+  sudo systemctl enable docker.service
+  sudo systemctl enable containerd.service
   apt-get update
   apt-get install curl
 ENDSSH
