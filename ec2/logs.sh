087ae12fcfc1c6f5b4cc6740d8ce51ab0effbef7
Pedro Gutierrez <pedro.gutierrez@avaldigitalabs.com>
2022-09-05 15:24:34 -0500

    :sparkles: improving logs script

--- ec2/logs.sh
+++ ec2/logs.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-sh ./git.sh
 ssh -i "$KEY" "ec2-user@$EC2" <<'ENDSSH'
    #commands to run on remote host
-  docker logs --tail 100 proxy
+  docker logs --follow --tail 100 proxy
 ENDSSH
