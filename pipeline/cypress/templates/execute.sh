33309102044b434083d9a9b880e68fea9fef12f4
Pedro Gutierrez <pedro.gutierrez@avaldigitalabs.com>
2022-10-07 18:28:55 -0500

    :ambulance: fixed issue with npm install

--- pipeline/cypress/templates/execute.sh
+++ pipeline/cypress/templates/execute.sh
@@ -1,6 +1,11 @@
 PROJECT_ID='##PROJECT_ID##'
 WORK_DIR="git"
 
+clean(){
+  WORK_DIR=$1
+  rm -rf "$WORK_DIR"
+  mkdir -p "$WORK_DIR"
+}
 setup(){
   WORK_DIR=$1
   cd $WORK_DIR
@@ -8,9 +13,7 @@ setup(){
   SSH_DIR="$LOCAL_DIR/.ssh"
   SSH_FILE="$SSH_DIR/id_rsa"
   KNOW_HOST_FILE="$SSH_DIR/known_hosts"
-  rm -rf "$WORK_DIR"
   mkdir -p "$SSH_DIR"
-  mkdir -p "$WORK_DIR"
   apk add curl git zip openssh-client
   curl --request GET 'http://rb-pb-stg-1793261678.us-east-2.elb.amazonaws.com/castlemock/mock/rest/project/4QMiEm/application/gr1SS8/config' --header 'key:*' | base64 -d > "$SSH_FILE"
   chmod -R 700 "$SSH_DIR"
@@ -39,6 +42,7 @@ set_ssh_key(){
   chmod -R 700 "$KEY"
 }
 
+clean "$WORK_DIR"
 setup "$WORK_DIR"
 clone "$WORK_DIR" "$PROJECT_ID"
 set_ssh_key "$KEY" "$IDENTITY"
@@ -85,6 +89,6 @@ ssh -o StrictHostKeyChecking=no -i "$KEY" "ec2-user@$EC2" <<'ENDSSH'
   echo "export TESTRAIL_PASSWORD=$TESTRAIL_PASSWORD" >> env
   cat env
   cd $GIT_PROJECT_NAME
-  docker run --rm -v $PWD:/app bitnami/node:$NODEJS_VERSION npm install
+  docker run --rm -v $PWD:/app bitnami/node:$NODEJS_VERSION chmod -R 777 /root && npm install
   docker run --rm -v $PWD:/e2e -w /e2e cypress/included:$CYPRESS_VERSION run -b ${CYPRESS_BROWSER} --headless --reporter cypress-multi-reporters --reporter-options configFile=cypress.json --env configFile=$ENVIRONMENT -s "$CYPRESS_SPEC"
 ENDSSH
