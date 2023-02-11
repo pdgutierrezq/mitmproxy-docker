a0c1ea8c5fed5f25e3d4825bc1601bbca43ec8b3
Pedro Gutierrez <pedro.gutierrez@avaldigitalabs.com>
2023-02-11 17:50:42 -0500

    🐛 fixed issue with unzip command line

--- pipeline/cypress/templates/closure.sh
+++ pipeline/cypress/templates/closure.sh
@@ -12,7 +12,7 @@ ssh -o StrictHostKeyChecking=no -i "$KEY_PATH" "ec2-user@$EC2" <<'ENDSSH'
   . "$WORK_DIR/env"
   REPORT_ABSOLUTE_PATH="$WORK_DIR/$GIT_PROJECT_NAME/$REPORT_PATH"
   cd "$WORK_DIR/$GIT_PROJECT_NAME"
-  docker run --rm -v $PWD:/app -w /app -e TESTRAIL_TESTRUN_NAME="$TESTRAIL_TESTRUN_NAME" -e TESTRAIL_USERNAME=$TESTRAIL_USERNAME -e TESTRAIL_PASSWORD=$TESTRAIL_PASSWORD node:$NODEJS_VERSION /bin/bash -c "apt update && apt-get install -y software-properties-common && add-apt-repository 'deb http://security.debian.org/debian-security stretch/updates main' && apt update && apt install -y openjdk-8-jdk && npm run test:reports"
+  docker run --rm -v $PWD:/app -w /app -e TESTRAIL_TESTRUN_NAME="$TESTRAIL_TESTRUN_NAME" -e TESTRAIL_USERNAME=$TESTRAIL_USERNAME -e TESTRAIL_PASSWORD=$TESTRAIL_PASSWORD node:$NODEJS_VERSION /bin/bash -c "apt update && apt-get install -y software-properties-common && add-apt-repository 'deb http://security.debian.org/debian-security stretch/updates main' && apt update && apt install -y openjdk-8-jdk && chmod -R 777 /root/.npm && chmod -R 777 $WORK_DIR && npm run test:reports"
   cd $REPORT_ABSOLUTE_PATH
   REPORT_INDEX_FILE=$(find . -type f -regex ".*.html" | head -n 1)
   echo "[INFO] REPORT PATH: $WORK_DIR/$GIT_PROJECT_NAME/$REPORT_PATH"
@@ -29,5 +29,5 @@ PIPELINE_TARGET_PATH="./target/site/serenity"
 scp -i "$KEY_PATH" "ec2-user@$EC2:$SRC_PATH" "$TARGET_PATH"
 rm -rf "$PIPELINE_TARGET_PATH"
 mkdir -p "$PIPELINE_TARGET_PATH"
-unzip "$TARGET_PATH/$FILE_NAME" -dq "$PIPELINE_TARGET_PATH"
+unzip -q "$TARGET_PATH/$FILE_NAME" -d "$PIPELINE_TARGET_PATH"
 ls -Rl $PIPELINE_TARGET_PATH
