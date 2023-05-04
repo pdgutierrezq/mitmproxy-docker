695cd3e725712f7153da190b8b197024259797f5
Pedro Gutierrez <pedro.gutierrez@avaldigitalabs.com>
2023-05-04 14:45:42 -0500

    improved runner

--- pipeline/cypress/templates/cron.sh
+++ pipeline/cypress/templates/cron.sh
@@ -8,14 +8,15 @@ set_ssh_key(){
 }
 set_ssh_key "$KEY_PATH" "$IDENTITY"
 FILE_NAME="job.sh"
-SRC_PATH="/home/ec2-user/jenkins"
+SRC_PATH="/home/ec2-user/local"
 EC2_MACHINE="ec2-user@$EC2"
+
 scp -i "$KEY_PATH" "$ROOT_DIR/$FILE_NAME" "$EC2_MACHINE:$SRC_PATH"
 ssh -o StrictHostKeyChecking=no -i "$KEY_PATH" "ec2-user@$EC2" <<'ENDSSH'
-  WORK_DIR="/home/ec2-user/jenkins"
-  chmod 777 /home/ec2-user/jenkins/job.sh
+  WORK_DIR="/home/ec2-user/local"
+  chmod 777 /home/ec2-user/local/job.sh
   crontab -l
-  echo '0 * * * * /home/ec2-user/jenkins/job.sh &> /home/ec2-user/log/`date +\%H-\%M-\%S_\%Y-\%m-\%d`.log' > cronjob
+  echo '0 * * * * /home/ec2-user/local/job.sh &> /home/ec2-user/log/`date +\%Y-\%m-\%d_\%H-\%M-\%S`.log' > cronjob
   crontab cronjob
   crontab -l
   rm cronjob
--- pipeline/cypress/templates/runner.sh
+++ pipeline/cypress/templates/runner.sh
@@ -1,19 +1,33 @@
 ROOT_DIR="$PWD"
 KEY_PATH="$ROOT_DIR/$KEY"
+EC2_MACHINE="ec2-user@$EC2"
 set_ssh_key(){
   KEY_PATH=$1
   IDENTITY=$2
   echo $IDENTITY | base64 -d > "$KEY_PATH"
   chmod -R 700 "$KEY_PATH"
 }
+copy_dir(){
+  ZIP_FILE_NAME=$1
+  SRC_PATH=$2
+  TARGET_PATH=$3
+  zip $ZIP_FILE_NAME -rq .
+  scp -i "$KEY_PATH" "$ROOT_DIR/$ZIP_FILE_NAME" "$EC2_MACHINE:$TARGET_PATH"
+  rm "$ROOT_DIR/$ZIP_FILE_NAME"
+}
 set_ssh_key "$KEY_PATH" "$IDENTITY"
-
-FILE_NAME="job.sh"
-SRC_PATH="/home/ec2-user/jenkins"
-EC2_MACHINE="ec2-user@$EC2"
-#scp -i "$KEY_PATH" "$EC2_MACHINE:$SRC_PATH/$FILE_NAME" "$ROOT_DIR"
-scp -i "$KEY_PATH" "$ROOT_DIR/$FILE_NAME" "$EC2_MACHINE:$SRC_PATH"
+copy_dir "local.zip" "." "/home/ec2-user"
 ssh -o StrictHostKeyChecking=no -i "$KEY_PATH" "ec2-user@$EC2" <<'ENDSSH'
-  WORK_DIR="/home/ec2-user/jenkins"
-  . $WORK_DIR/job.sh
+clean_dir(){
+    DIR=$1
+    sudo rm -rf "$WORK_DIR"
+    mkdir -p "$WORK_DIR"
+}
+  RELATIVE_DIR="local"
+  ROOT_DIR="/home/ec2-user"
+  WORK_DIR="$ROOT_DIR/$RELATIVE_DIR"
+  clean_dir "$WORK_DIR"
+  unzip -q "$ROOT_DIR/$RELATIVE_DIR.zip" -d "./$RELATIVE_DIR"
+  rm "$ROOT_DIR/$RELATIVE_DIR.zip"
+#  . $WORK_DIR/job.sh
 ENDSSH
