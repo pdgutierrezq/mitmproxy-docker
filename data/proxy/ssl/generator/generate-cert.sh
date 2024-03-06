b086268dd95a99ff9af1a30760290ee74daf4677
Pedro Gutierrez <pedro.gutierrez@avaldigitalabs.com>
2024-03-06 18:07:36 -0500

    ⚡️Cleaning data/proxy/ssl folder

--- data/proxy/ssl/generator/generate-cert.sh
+++ data/proxy/ssl/generator/generate-cert.sh
@@ -1,6 +1,6 @@
-OUTPUT_FILE=client_certs/apicapi-gateway-qa.appsptqa.ath.com.co.pem
-KEY_FILE=rb-stg-pb.avaldigitallabs.com.key
-CERT_FILE=bavv.cer
+OUTPUT_FILE=../client_certs/apicapi-gateway-qa.appsptqa.ath.com.co.pem
+KEY_FILE=data/rb-stg-pb.avaldigitallabs.com.key
+CERT_FILE=data/bavv.cer
 openssl rsa -in $KEY_FILE -passin file:noPass.txt -out decrypted_private_key.pem
 openssl rsa -in decrypted_private_key.pem -passin file:noPass.txt -out rsa_private_key.pem
 cp rsa_private_key.pem $OUTPUT_FILE
