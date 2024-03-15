258a404915b9e0f2b80579a105ed79b288751be1
Pedro Gutierrez <pedro.gutierrez@avaldigitalabs.com>
2024-03-15 18:06:43 -0500

    🚑fixed issue with script, missing semicolons

--- pipeline/cypress/templates/cypress/report/script.js
+++ pipeline/cypress/templates/cypress/report/script.js
@@ -14,14 +14,14 @@ function loadPage(pageUrl) {
 }
 
 function changeIframeSrc(iframeId) {
-  const baseUrl = "https://jenkins.rb-pre-ops.net/view/bavv-pasivo-qa/job/"
+  const baseUrl = "https://jenkins.rb-pre-ops.net/view/bavv-pasivo-qa/job/";
   const input = baseUrl + document.getElementById(
       `input` + iframeId.slice(-1)).value;
   document.getElementById(iframeId).src = input;
 }
 
 function reloadIframes() {
-  const baseUrl = "https://jenkins.rb-pre-ops.net/view/bavv-pasivo-qa/job/"
+  const baseUrl = "https://jenkins.rb-pre-ops.net/view/bavv-pasivo-qa/job/";
   for (let i = 1; i <= 7; i++) {
     document.getElementById(`iframe` + i).src = baseUrl
         + document.getElementById(`input` + i).value;
