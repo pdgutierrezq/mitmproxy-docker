3351ee42da3a80f202769402ee7517540092e195
Pedro Gutierrez <pedro.gutierrez@avaldigitalabs.com>
2024-03-15 17:20:39 -0500

    🚑 fixed issue with script on cypress report

--- pipeline/cypress/templates/cypress/report/script.js
+++ pipeline/cypress/templates/cypress/report/script.js
@@ -3,6 +3,7 @@ function loadPage(pageUrl) {
   .then(response => response.text())
   .then(data => {
     document.getElementById(`content`).innerHTML = data;
+    reloadIframes();
   });
 }
 
@@ -11,15 +12,7 @@ function changeIframeSrc(iframeId) {
   document.getElementById(iframeId).src = input;
 }
 
-function loadPage(pageUrl) {
-  fetch(pageUrl)
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById(`content`).innerHTML = data;
-  });
-}
-
-window.onload = function() {
+function reloadIframes(){
   document.getElementById(`iframe1`).src = document.getElementById(`input1`).value;
   document.getElementById(`iframe2`).src = document.getElementById(`input2`).value;
   document.getElementById(`iframe3`).src = document.getElementById(`input3`).value;
@@ -27,5 +20,9 @@ window.onload = function() {
   document.getElementById(`iframe5`).src = document.getElementById(`input5`).value;
   document.getElementById(`iframe6`).src = document.getElementById(`input6`).value;
   document.getElementById(`iframe7`).src = document.getElementById(`input7`).value;
+}
+
+window.onload = function() {
   loadPage(`dev.html`)
+  reloadIframes();
 };
