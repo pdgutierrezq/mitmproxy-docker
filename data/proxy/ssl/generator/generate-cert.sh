b3bb6f2306d5c3429f71e7ef1d080f95963b2238
Pedro Gutierrez <pedro.gutierrez@avaldigitalabs.com>
2024-04-02 11:31:18 -0500

    ⚡ Improved script

--- data/proxy/ssl/generator/generate-cert.sh
+++ data/proxy/ssl/generator/generate-cert.sh
@@ -1,6 +1,8 @@
 OUTPUT_FILE=../client_certs/apicapi-gateway-qa.appsptqa.ath.com.co.pem
 KEY_FILE=data/rb-stg-pb.avaldigitallabs.com.key
 CERT_FILE=data/bavv.cer
+ROOT_DIR="$PWD"
+cd $ROOT_DIR/data/proxy/ssl/generator
 openssl rsa -in $KEY_FILE -passin file:noPass.txt -out decrypted_private_key.pem
 openssl rsa -in decrypted_private_key.pem -passin file:noPass.txt -out rsa_private_key.pem
 cp rsa_private_key.pem $OUTPUT_FILE
